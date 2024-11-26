a4b88f6118e2efd2e95e2ad0b7eb89dc97ddfcb0
Tusharjagi <tusharjagi@gmail.com>
2024-11-27 00:11:01 +0530

    Auto Increment Counter

--- Auto Increment Counter/src/App.tsx
+++ Auto Increment Counter/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Setup</h1>
+        <h1>Auto Increment Counter</h1>
       </header>
     </div>
   );
