4b21284df54ce53132b1ee7bfaec1ca0a936e616
Tusharjagi <tusharjagi@gmail.com>
2024-11-27 00:06:15 +0530

    initial commit

---
+++ Carousel/src/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error(
+    "Root element not found. Please ensure there is an element with ID 'root' in your HTML."
+  );
+}
