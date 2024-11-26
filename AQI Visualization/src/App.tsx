d7116c50bb8f837fc66129982d3b22606e2a972a
Tusharjagi <tusharjagi@gmail.com>
2024-11-27 00:09:42 +0530

    AQI Visualization

--- AQI Visualization/src/App.tsx
+++ AQI Visualization/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Setup</h1>
+        <h1>AQI Visualization</h1>
       </header>
     </div>
   );
