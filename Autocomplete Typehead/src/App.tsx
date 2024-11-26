cbbf18b556c665f3a5046e114aa36cb0181ce552
Tusharjagi <tusharjagi@gmail.com>
2024-11-27 00:12:25 +0530

    Autocomplete Type Head

--- Autocomplete Typehead/src/App.tsx
+++ Autocomplete Typehead/src/App.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Setup</h1>
+        <h1>Autocomplete Type head</h1>
       </header>
     </div>
   );
